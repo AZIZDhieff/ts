0e0b25b22d451a3bc52b677bcfa871060055e663
dhief aziz <dhiefaziz80@gmail.com>
2024-07-19 13:48:43 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var Car = /** @class */ (function () {
+    function Car(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    Car.prototype.start = function () {
+        console.log("Car engine started");
+    };
+    return Car;
+}());
+var myCar = new Car("hyundai", "accent", 2024);
+myCar.start();
+console.log(myCar);
